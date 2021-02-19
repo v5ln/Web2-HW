cdfd6423bdebda9066df9d9d834b34da74cb9549
v5ln <68195919+v5ln@users.noreply.github.com>
2021-02-19 21:11:55 +0300

    functionality change
    i change the functionality of the table
    (add/delete)

--- HW1/js/script.js
+++ HW1/js/script.js
@@ -1,12 +1,11 @@
 var students = [];
-var counter = 0;
 function GetTheData(){
     let nameInputEl = document.getElementById('name').value;
     let idInputEl = document.getElementById('idNumber').value;
     let gdpaInputEl = document.getElementById('gdpa').value;
     // Validation for input
     inputValidation(nameInputEl, idInputEl, gdpaInputEl);
-};
+}
 
 
 function inputValidation(name, id, gdpa) {
@@ -37,26 +36,18 @@ function inputValidation(name, id, gdpa) {
     // insert student
     insertStudent(name, id, gdpa);
 
-    // Show success message
-    //showMessage('success');
 }
 
-function showMessage(event){
-    if (event == 'success') {
-        alert('Studnet added!')
-    }else{
-        alert('Faild to add student')
-    }
-}
 
 function Delete(){
-    var to_delete = document.getElementById("Del").value;
+    var to_delete = document.getElementById("Del-Upd").value;
     for (var i=0; i<students.length; i++){
         if (students[i].id == to_delete){
             students.splice(i,1);
             console.log('students array: ', students);
         }
     }
+    table();
 }
 
 
@@ -66,52 +57,25 @@ function insertStudent(name, id, gdpa) {
         name: name,
         gdpa: gdpa,
     };
-
-    var new_row = $("<tr>");
-    var new_col = "";
-
-    new_col += "<td><p class='addedName'></p></td>";
-    new_col += "<td><p class='addedID'></p></td>";
-    new_col += "<td><p class='addedGDPA'></p></td>";
-    //new_col += '<td><input type="button" class="ibtnDel btn btn-md btn-danger "  value="Delete"></td>';
-
-
-    new_row.append(new_col);
-    $("table.order-list").append(new_row);
-    document.getElementsByClassName("addedName")[counter].innerHTML = name;
-    document.getElementsByClassName("addedID")[counter].innerHTML = id;
-    document.getElementsByClassName("addedGDPA")[counter].innerHTML = gdpa;
     students.push(student);
     console.log('students array: ', students);
-    counter++;
+    table();
 }
 
-
-
-
-$(document).ready(function () {
-
-
-    $("#addrow").on("click", function () {
-        var newRow = $("<tr>");
-        var cols = "";
-
-        cols += '<td><input type="text" class="form-control" name="name' + counter + '"/></td>';
-        cols += '<td><input type="text" class="form-control" name="mail' + counter + '"/></td>';
-        cols += '<td><input type="text" class="form-control" name="phone' + counter + '"/></td>';
-
-        //cols += '<td><input type="button" class="ibtnDel btn btn-md btn-danger "  value="Delete"></td>';
-        newRow.append(cols);
-        $("table.order-list").append(newRow);
-        counter++;
-    });
-
-
-
-    //$("table.order-list").on("click", ".ibtnDel", function (event) {
-    //    $(this).closest("tr").remove();
-    //    counter -= 1
-    //});
-
-
-});
+function table(){
+    var html = '<table class=" table order-list">';
+    html += '<tr>\n' +
+        '            <td>ID</td>\n' +
+        '            <td>Name</td>\n' +
+        '            <td>GDPA</td>\n' +
+        '        </tr>'
+    for (var i=0; i< students.length; i++){
+        html += '<tr>';
+        html += '<td>' + students[i].id + '</td>';
+        html += '<td>' + students[i].name + '</td>';
+        html += '<td>' + students[i].gdpa + '</td>';
+        html += '</tr>';
+    }
+    html += '</table>';
+    document.getElementById('table').innerHTML = html;
+}
