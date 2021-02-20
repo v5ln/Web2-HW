61a269471717dcc0405016387344eafd4ad69aca
v5ln <68195919+v5ln@users.noreply.github.com>
2021-02-20 11:20:49 +0300

    final commit
    - build update functionality
    - build the update modal
    - debugging
    - final touch

--- HW1/js/script.js
+++ HW1/js/script.js
@@ -7,7 +7,6 @@ function GetTheData(){
     inputValidation(nameInputEl, idInputEl, gdpaInputEl);
 }
 
-
 function inputValidation(name, id, gdpa) {
     // check for the value of each element
 
@@ -38,18 +37,18 @@ function inputValidation(name, id, gdpa) {
 
 }
 
-
 function Delete(){
     var to_delete = document.getElementById("Del-Upd").value;
     for (var i=0; i<students.length; i++){
         if (students[i].id == to_delete){
             students.splice(i,1);
             console.log('students array: ', students);
+            table();
+            return;
         }
     }
-    table();
+    alert('ID is not there !');
 }
-
 
 function insertStudent(name, id, gdpa) {
     let student = {
@@ -78,4 +77,41 @@ function table(){
     }
     html += '</table>';
     document.getElementById('table').innerHTML = html;
+}
+
+var modal = document.getElementById("myModal");
+function Update(){
+    var updateID = document.getElementById('Del-Upd').value;
+    for (var i=0; i<students.length; i++){
+        if (students[i].id == updateID){
+            modal.style.display = "block";
+
+            document.getElementById('UpdateGDPA').value = students[i].gdpa;
+            document.getElementById('UpdateName').value = students[i].name;
+
+            var submit = document.getElementById('UpdateDone');
+            submit.onclick = function (){
+
+                students[i].gdpa = document.getElementById('UpdateGDPA').value;
+                students[i].name = document.getElementById('UpdateName').value;
+
+                modal.style.display = "none";
+                console.log('students array: ', students);
+                table();
+            }
+            return;
+        }
+    }
+    alert('ID is not there !');
+}
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
